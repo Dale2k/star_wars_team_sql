7cef53048ce2b07690215fb5df3a95595dece89c
Darryl Subal <dsubal@att.net>
2023-04-27 09:03:03 -0400

    add enquirerer questions

--- index.js
+++ index.js
@@ -21,3 +21,54 @@ connection.connect(function (err) {
   console.log("Connected as Id");
   startPrompt();
 });
+
+function startPrompt() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "Select from below please",
+        name: "choice",
+        choices: [
+          "View Employees",
+          "View Employees By Role",
+          "View Employees By Department",
+          "Update Employee",
+          "Add Employee",
+          "Add Role",
+          "Add Department",
+        ],
+      },
+      //functions
+    ])
+    .then(function (val) {
+      switch (val.choice) {
+        case "View Employees":
+          viewEmployees();
+          break;
+
+        case "View Employees By Role":
+          viewRoles();
+          break;
+        case "View Employees By Department":
+          viewDepartments();
+          break;
+
+        case "Add Employee":
+          addEmployee();
+          break;
+
+        case "Update Employee":
+          updateEmployee();
+          break;
+
+        case "Add Role":
+          addRole();
+          break;
+
+        case "Add Department":
+          addDepartment();
+          break;
+      }
+    });
+}
