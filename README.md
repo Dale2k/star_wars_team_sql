dcf781c0f53ad20aa450a8f007ba92fa04042bac
Darryl Subal <dsubal@att.net>
2023-04-28 04:18:15 -0400

    add addl routing functions

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # star_wars_team_sql
+
+
+- The URL of the GitHub repository,
+
+[https://github.com/Dale2k/ecom_database](https://github.com/Dale2k/ecom_database)
+
+https://github.com/Dale2k/ecom_database
+
+
+
+[Walktrough_video link](https://drive.google.com/file/d/1yw9FlTR5lQ6FTIPG8M9pszbRzU35aq2e/view)
+
+
+
+## Description
+
+To build the back end for an e-commerce. To configure a working Express.js API to use Sequelize to interact with a MySQL database.
+
+## User Story
+
+AS A manager at an internet retail company
+I WANT a back end for my e-commerce website that uses the latest technologies
+SO THAT my company can compete with other e-commerce companies
+
+
+## Installation
+
+
+Start mysql server 
+use  source schema.sql in mysql cli
+Npm install
+Npm Start index.js
+
+
+## License
+
+The Application is covered under MIT license. Learn more about the license from the following link: [https://choosealicense.com/licenses/mit](https://choosealicense.com/licenses/mit)
+
+## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]
+
+## Questions
+
+GitHub: dale2k. [Click here](https://github.com/dale2k) to go to my github profile
+
+For additional questions reach me at dsubal@att.net
--- index.js
+++ index.js
@@ -33,7 +33,6 @@ function startPrompt() {
           "View Employees",
           "View Employees By Role",
           "View Employees By Department",
-          "Update Employee",
           "Add Employee",
           "Add Role",
           "Add Department",
@@ -97,6 +96,7 @@ function viewRoles() {
   );
 }
 
+// view departments
 function viewDepartments() {
   connection.query(
     "SELECT employee.first_name, employee.last_name, department.name AS Department FROM employee JOIN role ON employee.role_id = role.id JOIN department ON role.department_id = department.id ORDER BY employee.id;",
@@ -106,4 +106,138 @@ function viewDepartments() {
       startPrompt();
     }
   );
+}
+
+//add employee
+function addEmployee() {
+  inquirer
+    .prompt([
+      {
+        name: "firstname",
+        type: "input",
+        message: "First name ",
+      },
+      {
+        name: "lastname",
+        type: "input",
+        message: "Last name ",
+      },
+      {
+        name: "role",
+        type: "list",
+        message: "What is their role? ",
+        choices: selectRole(),
+      },
+      {
+        name: "choice",
+        type: "rawlist",
+        message: "Whats their managers name?",
+        choices: selectManager(),
+      },
+    ])
+    .then(function (val) {
+      var roleId = selectRole().indexOf(val.role) + 1;
+      var managerId = selectManager().indexOf(val.choice) + 1;
+      connection.query(
+        "INSERT INTO employee SET ?",
+        {
+          first_name: val.firstName,
+          last_name: val.lastName,
+          manager_id: managerId,
+          role_id: roleId,
+        },
+        function (err) {
+          if (err) throw err;
+          console.table(val);
+          startPrompt();
+        }
+      );
+    });
+}
+
+let mgrSet = [];
+function selectManager() {
+  connection.query(
+    "SELECT first_name, last_name FROM employee WHERE manager_id IS NULL",
+    function (err, res) {
+      if (err) throw err;
+      for (var i = 0; i < res.length; i++) {
+        mgrSet.push(res[i].first_name);
+      }
+    }
+  );
+  return mgrSet;
+}
+
+// add role
+function addRole() {
+  connection.query(
+    "SELECT role.title AS Title, role.salary AS Salary FROM role",
+    function (err, res) {
+      inquirer
+        .prompt([
+          {
+            name: "Title",
+            type: "input",
+            message: "What is their Title?",
+          },
+          {
+            name: "Salary",
+            type: "input",
+            message: "What is their Salary?",
+          },
+        ])
+        .then(function (res) {
+          connection.query(
+            "INSERT INTO role SET ?",
+            {
+              title: res.Title,
+              salary: res.Salary,
+            },
+            function (err) {
+              if (err) throw err;
+              console.table(res);
+              startPrompt();
+            }
+          );
+        });
+    }
+  );
+}
+
+// add department
+
+function addDepartment() {
+  inquirer
+    .prompt([
+      {
+        name: "name",
+        type: "input",
+        message: "What Department to add?",
+      },
+    ])
+    .then(function (res) {
+      var query = connection.query(
+        "INSERT INTO department SET ? ",
+        {
+          name: res.name,
+        },
+        function (err) {
+          if (err) throw err;
+          console.table(res);
+          startPrompt();
+        }
+      );
+    });
+}
+
+var roleArr = [];
+function selectRole() {
+  connection.query("SELECT * FROM role", function (err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      roleArr.push(res[i].title);
+    }
+  });
+  return roleArr;
 }
